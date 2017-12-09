170ba1493253ef5a9812168ec0a68289fa5387f8
kosmo04 <tt>
2017-12-09 10:45:17 +0900

    Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HomeComponent } from './home/home.component';
+import { JqueryComponent } from './jquery/jquery.component';
+import { AngularComponent } from './angular/angular.component';
+import { AppRoutiongModule } from './/app-routiong.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HomeComponent,
+    JqueryComponent,
+    AngularComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutiongModule
   ],
   providers: [],
   bootstrap: [AppComponent]
